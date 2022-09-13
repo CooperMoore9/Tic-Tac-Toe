63aed809255ea8ea05746c209551a72de0b29890
CooperMoore9 <thefryman9@gmail.com>
2022-09-13 14:24:12 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,13 +1,3 @@
-// store the gameboard as an array inside of a Gameboard object
-// players are also going to be stored in objects
-// want an object to control the flow of the game itself
-
-// Your main goal here is to have as little global code as possible. Try tucking everything away inside of a module or factory. Rule of thumb: if you only ever need ONE of something (gameBoard, displayController), use a module. If you need multiples of something (players!), create them with factories.
-
-// correlate buttons on html to javascript buttons, when you click the buttons add it into the array where the button is
-
-// Possible solution to GameState; Every time a button is clicked, check the text context of the board, check which class it has then add it to the array at the class of the html element
-
 const gameBoard = (() => {
 
   const WINNING_MOVES = 
